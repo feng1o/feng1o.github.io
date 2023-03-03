9c6173b8f0332ad50baa28fbd5bb048d1175b67d
joxliu <joxliu@tencent.com>
2023-03-03 18:57:59 +0800

    rebuilding site Fri Mar  3 18:57:54 CST 2023 add src to dev

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 # hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 # hugo serve --disableFastRender
+git checkout dev
+git submodule update
+
 hugo
 
 # Go To Public folder
